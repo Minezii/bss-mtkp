0089041e13b668404616d2be2b0c6286c9f016dc
Minezii <minezii@users.noreply.github.com>
2026-03-22 00:00:34 +0300

    Switch to CommonJS eslint.config.js for reliability

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const { dirname } = require("path");
+const { fileURLToPath } = require("url");
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+module.exports = eslintConfig;
